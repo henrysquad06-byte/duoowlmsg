03fb2653c0006f8fb775a5a3b52d1bde4e7e1201
henrysquad06-byte <henrysquad06@gmail.com>
2026-08-17 17:00:33 -0700

    Add happyMessages array with greeting messages

--- replys/happy.js
+++ replys/happy.js
@@ -1,1 +1,12 @@
-
+const happyMessages = [
+  "Good morning, Henry! 👋🦉",
+  "Hey bestie! I'm glad you're here! 💚",
+  "YAY! You're here! 🎉",
+  "Hello Henry! Duo reporting for duty! 🦉",
+  "It's good to see you! 😄",
+  "We're gonna have a great day! 💚🦉",
+  "Besties forever! 😎🦉",
+  "I'm ready! What are we doing today?",
+  "Oh hey! I was waiting for you! 👀",
+  "Let's have some fun today! 🎉"
+];
